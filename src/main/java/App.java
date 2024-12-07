0dfa5676aa446ba7365ac5c4b8431d6befc4d5d8
Melhi95 <melhi.92@mail.ru>
2024-12-07 08:25:36 +0300

    Удалено явное приведение в App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,15 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
 
-        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean2.getMessage());
 
-        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat = applicationContext.getBean("cat", Cat.class);
         System.out.println(cat.getName());
-        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
         System.out.println(cat2.getName());
 
         System.out.println("Equal: " + bean.equals(bean2));
